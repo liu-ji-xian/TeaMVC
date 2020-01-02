30743224bd4fa0f2849416dd09522bd04a9d7e09
刘继先 <18662765213@163.com>
2020-01-02 19:53:58 +0800

    GoodsType舍弃Model类命名空间加s

---
+++ TeaMVC/Controllers/GoodsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using TeaMVC.Models;
+
+namespace TeaMVC.Controllers
+{
+    public class GoodsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ShowGoods(string name)
+        {
+            string url;
+            if (name!=null)
+            {
+                url = "https://localhost:44346/Goods/ShowGoods?name=" + name;
+            }
+            else
+            {
+                url = "https://localhost:44346/Goods/ShowGoods";
+            }
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage httpRequest = httpClient.GetAsync(url).Result;
+            string str = httpRequest.Content.ReadAsStringAsync().Result;
+            List<Goods> list = JsonConvert.DeserializeObject<List<Goods>>(str);
+            return View(list);
+        }
+    }
+}
--- TeaMVC/Models/Orders.cs
+++ TeaMVC/Models/Orders.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace TeaMVC.Model
+namespace TeaMVC.Models
 {
     /// <summary>
     /// 订单表
